de040a6a392fb322c89eca2b60946b5b92619877
Jess DiPonzio <jsdiponzio@gmail.com>
2021-12-01 20:53:47 -0500

    Adding Day 1 Part 2

---
+++ day1/part2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *list = fopen("day 1a.txt", "r");
+    if (list == NULL)
+    {
+        return 1;
+    }
+    
+    const int SIZE = 2000;
+    int numbers[SIZE];
+    int larger = 0;
+    int i;
+
+    for (i = 0; i < SIZE; i++)
+    {
+        fscanf(list, "%i", &numbers[i]);
+    }
+    
+    for (i = 1; i < (SIZE - 2); i++)
+    {
+        int a = numbers[i];
+        int b = numbers[i + 1];
+        int c = numbers[i + 2];
+        int d = numbers[i + 3];
+
+        if (b + c + d > a + b + c)
+        {
+            larger++;
+        }
+    }
+    
+    printf("%i\n", larger);
+    
+    fclose(list);
+}
